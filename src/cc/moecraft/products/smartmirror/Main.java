369e247a4626ba82c94b74e8eeec3483a266ba40
kilpikonna <871674895@qq.com>
2017-04-08 22:35:51 -0400

    Fixed Chinese path error
    - Fixed Chinese path error

--- src/cc/moecraft/products/smartmirror/Main.java
+++ src/cc/moecraft/products/smartmirror/Main.java
@@ -1,5 +1,6 @@
 package cc.moecraft.products.smartmirror;
 
+import cc.moecraft.products.smartmirror.configuration.file.YamlConfiguration;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,13 +10,30 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.awt.*;
+import java.io.DataInput;
+import java.io.File;
+import java.io.IOException;
 
-public class Main extends Application {
+import static cc.moecraft.products.smartmirror.logger.Debug;
+import static cc.moecraft.products.smartmirror.logger.log;
+
+public class Main extends Application
+{
     public static boolean debug = true;
+    private static Main instance = null;
+
+    public static Main getInstance()
+    {
+        return instance;
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception
     {
+        instance = this;
+
+        checkConfig();
+
         Parent root = FXMLLoader.load(getClass().getResource("SmartMirror.fxml"));
         root.setStyle("-fx-background-color: #000000;");
 
@@ -32,7 +50,48 @@ public class Main extends Application {
         primaryStage.show();
     }
 
+    public static YamlConfiguration config;
+    public static File configFile;
+    public boolean checkConfig()
+    {
+        configFile = new File(this.getDataFolder() + "Config.yml");
+        Debug(getDataFolder());
+        config = YamlConfiguration.loadConfiguration(configFile);
+        config.options().copyDefaults(true);
+        if (!(config.contains("Generated")))
+        {
+            config.addDefault("Generated", true);
+            saveConfig();
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    public static boolean saveConfig()
+    {
+        try
+        {
+            config.save(configFile);
+            return true;
+        }
+        catch (IOException e1)
+        {
+            e1.printStackTrace();
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public String getDataFolder()
+    {
+        String path = System.getProperty("user.dir");
+        Debug("Got data folder: " + path);
+        return path;
     }
 }
