3d313ec860f38565b5e5fd6b2780c30bbed993e8
kilpikonna <871674895@qq.com>
2017-04-10 19:11:07 -0400

    Added more features (in description)

--- src/cc/moecraft/products/smartmirror/Main.java
+++ src/cc/moecraft/products/smartmirror/Main.java
@@ -4,21 +4,23 @@ import cc.moecraft.products.smartmirror.configuration.file.YamlConfiguration;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 
 import static cc.moecraft.products.smartmirror.logger.Debug;
+import static cc.moecraft.products.smartmirror.logger.WHITE;
+import static cc.moecraft.products.smartmirror.logger.log;
 
 public class Main extends Application
 {
     public static boolean debug = true;
     private static Main instance = null;
 
-    private Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    private java.awt.Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
     private double screenSizeWidth = screenSize.getWidth();
     private double screenSizeHeight = screenSize.getHeight();
     private double screenSizeMidX = screenSizeWidth / 2;
@@ -32,7 +34,7 @@ public class Main extends Application
     public static Pane root;
     public static Stage stage;
     public static Scene scene;
-
+    public static String fontCSS;
     @Override
     public void start(Stage stage) throws Exception
     {
@@ -40,10 +42,26 @@ public class Main extends Application
 
         checkConfig();
 
+        fontCSS = Main.class.getResource("css/Fonts.css").toExternalForm();
+
         root = FXMLLoader.load(getClass().getResource("SmartMirror.fxml"));
         root.setStyle("-fx-background-color: #000000");
-        root.getChildren().add(new DigitalClock());
-        root.getStylesheets().add(Main.class.getResource("style.css").toExternalForm());
+
+        Pane digitalClockPane = new Pane(new DigitalClock());
+        digitalClockPane.setLayoutX(config.getInt("DigitalClock.Position.X"));
+        digitalClockPane.setLayoutY(config.getInt("DigitalClock.Position.Y"));
+        digitalClockPane.getStylesheets().add(fontCSS);
+        digitalClockPane.setStyle(
+                        "-fx-font: "      + config.getInt("DigitalClock.Font.Size") + "pt \"" + config.getString("DigitalClock.Font.Name") + "\";" +
+                        "-fx-text-fill: rgb(" + config.getInt("DigitalClock.Font.Color.Red") + ", " + config.getInt("DigitalClock.Font.Color.Green") + ", " + config.getInt("DigitalClock.Font.Color.Blue") + ");"
+        );
+
+        Debug(
+                "-fx-font: "      + config.getInt("DigitalClock.Font.Size") + "pt \"" + config.getString("DigitalClock.Font.Name") + "\";" +
+                "-fx-text-fill: rgb(" + config.getInt("DigitalClock.Font.Color.Red") + ", " + config.getInt("DigitalClock.Font.Color.Green") + ", " + config.getInt("DigitalClock.Font.Color.Blue") + ");"
+        );
+
+        root.getChildren().add(digitalClockPane);
         scene = new Scene(root, screenSizeWidth, screenSizeHeight);
 
         Main.stage = stage;
@@ -59,20 +77,39 @@ public class Main extends Application
 
     public static YamlConfiguration config;
     public static File configFile;
+
+    /**
+     * 检查配置文件
+     * Check config file
+     * @return 是不是新的, 如果是返回False, 如果不是返回True
+     */
     public boolean checkConfig()
     {
+        log("[配置] 正在检查配置文件");
         configFile = new File(this.getDataFolder() + "Config.yml");
-        Debug(getDataFolder());
+        log("[配置] DataFolder = " + getDataFolder());
         config = YamlConfiguration.loadConfiguration(configFile);
         config.options().copyDefaults(true);
+
         if (!(config.contains("Generated")))
         {
+            log("[配置] 检测到配置文件是新的, 正在储存默认配置");
             config.addDefault("Generated", true);
+            //时钟
+            config.addDefault("DigitalClock.Position.X", 60);
+            config.addDefault("DigitalClock.Position.Y", 60);
+            config.addDefault("DigitalClock.Font.Size", 76);
+            config.addDefault("DigitalClock.Font.Name", "Microsoft Yahei UI Light");
+            config.addDefault("DigitalClock.Font.Color.Red", 255);
+            config.addDefault("DigitalClock.Font.Color.Green", 255);
+            config.addDefault("DigitalClock.Font.Color.Blue", 255);
+
             saveConfig();
             return false;
         }
         else
         {
+            log("[配置] 检测到配置文件不是新的");
             return true;
         }
     }
--- src/cc/moecraft/products/smartmirror/logger.java
+++ src/cc/moecraft/products/smartmirror/logger.java
@@ -9,14 +9,24 @@ public class logger
 {
     private static Logger log = Logger.getLogger(logger.class);
 
+    public static final String RESET = "\u001B[0m";
+    public static final String BLACK = "\u001B[30m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String PURPLE = "\u001B[35m";
+    public static final String CYAN = "\u001B[36m";
+    public static final String WHITE = "\u001B[37m";
+
     public static void Debug(String s)
     {
         if (debug)
         {
-            log("[DEBUG(" +
+            pureLog("[" + YELLOW + "DEBUG" + WHITE + "][" + CYAN +
                     Thread.currentThread().getStackTrace()[2].getClassName() + "." +
                     Thread.currentThread().getStackTrace()[2].getMethodName() + ":" +
-                    Thread.currentThread().getStackTrace()[2].getLineNumber() + ")] " + s);
+                    Thread.currentThread().getStackTrace()[2].getLineNumber() + WHITE + "] " + GREEN + stringFormat(s));
         }
     }
 
@@ -24,21 +34,46 @@ public class logger
     {
         if (debug)
         {
-            log("[DEBUG(" +
+            pureLog("[" + YELLOW + "DEBUG" + WHITE + "][" + CYAN +
                     Thread.currentThread().getStackTrace()[2].getClassName() + "." +
                     Thread.currentThread().getStackTrace()[2].getMethodName() + ":" +
-                    Thread.currentThread().getStackTrace()[2].getLineNumber() + ")] " +
-                    object.getClass().getSimpleName() + ": " + message);
+                    Thread.currentThread().getStackTrace()[2].getLineNumber() + WHITE + "][" + CYAN +
+                    object.getClass().getSimpleName() + WHITE + "] " + GREEN + stringFormat(message));
         }
     }
 
     public static void log(String s)
     {
-        log.info(s);
+        log(Level.INFO, s);
     }
 
     public static void log(Level l, String s)
     {
-        log.log(l, s);
+        log.log(l, WHITE + "[" + CYAN + "LOG" + WHITE + "] " + stringFormat(s));
+    }
+
+    private static void pureLog(String s)
+    {
+        log.info(s);
+    }
+
+    public static String stringFormat(String s)
+    {
+        String output = "";
+        for (int i = 0; i < s.length(); i++)
+        {
+            switch (s.charAt(i))
+            {
+                case '[':
+                    output += WHITE + "[" + CYAN;
+                    break;
+                case ']':
+                    output += WHITE + "]" + GREEN;
+                    break;
+                default:
+                    output += s.charAt(i);
+            }
+        }
+        return output;
     }
 }
